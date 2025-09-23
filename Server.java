949d34793af50ed28d198f06f0d55c4ee182f5f6
John Hightower <john.hightower@uafs.edu>
2025-09-23 16:03:46 -0500

    Updated

--- Server.java
+++ Server.java
@@ -17,14 +17,26 @@ public class Server{
     private void processConnection() throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(),true);
+        String buffer;
 
-        //*** Application Protocol *****
-        String buffer = in.readLine();
-        while(!(buffer.equals("quit"))){
-            out.println("From Server: " + buffer);
-            buffer = in.readLine();
+        /*****************************************
+         *  1. Read HTTP Request From Browser    *
+         *****************************************/
+        buffer=in.readLine();
+        while(buffer.length()!=0){  
+            System.out.printf("%s\n",buffer);
+            buffer=in.readLine();
         }
-       
+        System.out.printf("------------------------------\n");
+
+        /*****************************************
+         *  2. Write HTTP Response to Browser    *
+         *****************************************/
+        out.printf("HTTP/1.1 200 OK\n");
+        out.printf("Content-Type: text/html\n");
+        out.printf("Content-Length: 20\n\n");
+        out.printf("<h1>Hello World</h1>\n\n");
+
         in.close();
         out.close();
     }
